e0c48babc973d8a7d2868d61066794e22a281b41
Sofiane <rezzaq45@gmail.com>
2025-05-19 18:30:25 +0200

    creation de la collection medals

---
+++ models/medals.js
@@ -0,0 +1,16 @@
+//importation du module "mongoose"
+const mongoose = require('mongoose');
+
+//Schema du model de la Collection "Medals"
+const medalSchema = mongoose.Schema(
+{
+    title: String,
+    image: String,
+    activity:{ type: mongoose.Schema.Types.ObjectId, ref: 'activities' }
+
+ 
+});
+
+//exportation du model "Medal"
+const Medal = mongoose.model('medals', medalSchema);
+module.exports = Medal;
