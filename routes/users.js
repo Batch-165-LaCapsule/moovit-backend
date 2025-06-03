05caadb2ddb82cf4be50732cf3ed39ddd1a290c4
Sofiane <rezzaq45@gmail.com>
2025-06-03 14:00:19 +0200

    effacer tout le code commenté et ajout des commentaires pour expliquer le code

--- routes/users.js
+++ routes/users.js
@@ -3,32 +3,33 @@ var router = express.Router();
 require("../models/connection"); //import de la connection string
 const User = require("../models/users"); //import du schema user
 const Activity = require("../models/activities"); //import du schema activity
-const Medal = require("../models/medals")
-
 const { checkBody } = require("../modules/checkBody"); //import de la fonction checkBody qui verifie que tout le champs soit ni null ni une string vide
-const uid2 = require("uid2"); // module qui permet de genere une num de token
+const uid2 = require("uid2"); // module qui permet de generer une num de token
 const bcrypt = require("bcrypt"); //module permet de haché le password
 
+ //route post endpoint /signup
 router.post("/signup", (req, res) => {
-  //route post endpoint /signup
+ 
+  //fonction checkBody qui verifie que tout les champs soit ni null ni une string vide prend en parametre du body ['username', 'password']
   if (!checkBody(req.body, ["email", "password"])) {
-    //fonction checkBody qui verifie que tout le champs soit ni null ni une string vide prend en parametre du body ['username', 'password']
+    
     res.json({ result: false, error: "Missing or empty fields" }); //la fun renvoi un json resiltat false error: 'Missing or empty fields'
     return;
   }
 
-  // Check if the user has not already been registered
+   //recherche dans la db User UN seul User avec le nom en param
   User.findOne({ email: req.body.email }).then((data) => {
-    //recherche dans la db User UN seul User avec le nom en param
+   
     const emailRegex = /^[\w.-]+@[\w.-]+\.[a-z]{2,}$/i; //regex pattern d'email (([\w.-]=raccourci de [a-zA-Z0-9_.-])) @ et 2caractere apres le .
-    // console.log(emailRegex.test(req.body.email));
-
+    
+     //si il n'y a pas de data reçu et que le regex .test renvoie true
     if (data === null && emailRegex.test(req.body.email)) {
-      //si il n'y a pas de data reçu ou que le regex .test renvoie true
+     
       const hash = bcrypt.hashSync(req.body.password, 10); // cryptage du password (hashé 10x)
 
+       //creation d'un new user
       const newUser = new User({
-        //creation d'un new user
+       
         email: req.body.email, // inscription de l ecmail en db
         password: hash, // cryptage du password (hashé 10x)
         token: uid2(32), //token de 32 caracteres
@@ -36,7 +37,7 @@ router.post("/signup", (req, res) => {
 
       newUser
         .save() //methode crud create avec .save de new user
-        .catch((error) => {
+        .catch(() => {
           res.status(500).json({ error: "error while saving doc" }); // Si une erreur survient lors de la sauvegarde du document
         })
         .then((newDoc) => {
@@ -52,31 +53,22 @@ router.post("/signup", (req, res) => {
   });
 });
 
+//route post endpoint /signin
 router.post("/signin", (req, res) => {
-  //route post endpoint /signin
+  
+      //fonction checkBody qui verifie que tout le champs soit ni null ni une string vide prend en parametre du body ['username', 'password']
   if (!checkBody(req.body, ["email", "password"])) {
-    //fonction checkBody qui verifie que tout le champs soit ni null ni une string vide prend en parametre du body ['username', 'password']
     res.json({ result: false, error: "wrong or empty fields" }); //la fun renvoi un json resiltat false error: 'Missing or empty fields'
     return;
   }
 
-  User.findOne({ email: req.body.email }) //recherche en db de l email
+  User.findOne({ email: req.body.email }) //recherche en db de l email//recherche dans la db User UN seul User avec le nom en param et renvoi des donné le consernant
     .then((data) => {
-      //recherche dans la db User UN seul User avec le nom en param et renvoi des donné le consernant
+      //si data et password rentré et crypté est identique au password en db
       if (data && bcrypt.compareSync(req.body.password, data.password)) {
-        //si data et password rentré et crypté est identique au password en db
         
-        // res.json({
-        //   //renvoi de toute ces info de la bdd au front
-        //   result: true,
-        //   token: data.token,
-        //   sportPlayed: data.sportPlayed,
-        //   username: data.username,
-        //   admin: data.admin,
-        //   xp: data.xp,
-        //   level: data.level,
-        //   photoUrl: data.photoUrl,
-        // }); //renvoi un json avec un resultat true et le token
+        
+        
 
         res.json({result:true, token:data.token, sport: data.sportPlayed})//renvoi de la reponse si l'authentification a réussi
 
@@ -84,184 +76,13 @@ router.post("/signin", (req, res) => {
         res.json({ result: false, error: "User not found or wrong password" }); // renvoi un json resultat false et un msg error
       }
     })
-    .catch((error) => {
+    .catch(() => {
       res.status(500).json({ error: "connection data base error " }); // Si une erreur survient lors de la sauvegarde du document
     });
 });
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-// router.post('/form/p1', (req, res) => { //
-//   if (!checkBody(req.body, ['username', 'name'])) {
-//     res.json({ result: false, error: 'Missing or empty fields' });
-//     return;
-//   }
-//   User.updateOne({token: res.json() },
-//   {username: req.body.username, name:req.body.name}).then(()=>{User.find().then(data=>{console.log(data);
-//   }).
 
-//       res.send()
-//         // username: req.body.username, //username
-//         // name:req.body.name
 
-// })})
-
-// router.post('/form/p2', (req, res) => { //
-//   if (!checkBody(req.body, ['gender'])) {
-//     res.json({ result: false, error: 'Missing or empty fields' });
-//     return;
-//   }
-//  User.updateOne({token:res.json() },
-//   {gender:req.body.gender}).then(()=>{User.find().then (data=>{console.log(data);
-//   }).
-//        // gender:req.body.gender
-
-// })})
-
-// router.post('/form/p3', (req, res) => { //
-//   if (!checkBody(req.body, ['age', 'city'])) {
-//     res.json({ result: false, error: 'Missing or empty fields' });
-//     return;
-//   }
-//   User.updateOne({token:res.json() },
-//   {age: req.body.age, city:req.body.city }).then(()=>{User.find().then(data=>{console.log(data);
-//   }).
-
-//         // age: req.body.age
-//         // // coordinate:req.body
-//         // city:req.body.city
-
-// })})
-
-// router.post('/form/p4', (req, res) => { //
-//   if (!checkBody(req.body, ['sportplayed'])) {
-//     res.json({ result: false, error: 'Missing or empty fields' });
-//     return;
-//   }
-//   User.updateOne({token:res.json() },
-//   { sportplayed:req.body.sportplayed}).then(()=>{User.find().then(data=>{console.log(data);
-//   }).
-
-//        // sportplayed:req.body.sportplayed
-
-// })})
-
-// router.post('/form/p5', (req, res) => { //
-//   if (!checkBody(req.body, ['sportStartingLevel'])) {
-//     res.json({ result: false, error: 'Missing or empty fields' });
-//     return;
-//   }
-//    User.updateOne({token:res.json() },
-//   { sportStartingLevel:req.body.sportStartingLevel}).then(()=>{User.find().then(data=>{console.log(data);
-//   }).
-
-//        // sportStartingLevel:req.body.sportStartingLevel
-
-// })})
-
-// router.post('/form/p6', (req, res) => { //
-//   if (!checkBody(req.body, ['coachNeed'])) {
-//     res.json({ result: false, error: 'Missing or empty fields' });
-//     return;
-//   }
-//    User.updateOne({token:res.json() },
-//   { coachNeed:req.body.coachNeed}).then(()=>{User.find().then(data=>{console.log(data)
-//   }).
-
-//        // coachNeed:req.body.coachNeed
-
-// })})
-
-// router.post('/form/p7', (req, res) => { //
-//   if (!checkBody(req.body, ['setFreeTime'])) {
-//     res.json({ result: false, error: 'Missing or empty fields' });
-//     return;
-//   }
-//    User.updateOne({token:res.json() },
-//   {setFreeTime:req.body.setFreeTime}).then(()=>{User.find().then(data=>{console.log(data);
-//   }).
-
-//        // setFreeTime:req.body.setFreeTime
-
-// })})
-
-// router.post('/form/p8', (req, res) => { //
-//   if (!checkBody(req.body, ['notificationActive'])) {
-//     res.json({ result: false, error: 'Missing or empty fields' });
-//     return;
-//   }
-//    User.updateOne({token:res.json() },
-//   {notificationActive:req.body.notificationActive}).then(()=>{User.find().then(data=>{console.log(data);
-//   }).
-
-//        // notificationActive:req.body.notificationActive
-
-// })})
-
-//  router.post('/signin', (req, res) => { //
-//  if (!checkBody(req.body, ['email', 'password'])) {
-//     res.json({ result: false, error: 'Missing or empty fields' });
-//     return;
-//   }
-//   User.findOne({ email: req.body.email }).then(data => { //recherche dans la db User UN seul User avec le nom en param et renvoi des donné le consernant
-//     if (data && bcrypt.compareSync(req.body.password, data.password)) { //si data et password rentré et crypté est identique au password en db
-//       res.json({ result: true, token: data.token }); //renvoi un json avec un resultat true et le token
-//     } else {
-//       res.json({ result: false, error: 'User not found or wrong password' });// renvoi un json resultat false et un msg error
-//     }
-//   });
-// });
-
-// router.post('/signin/lost', (req, res) => { //pasword perdu
-//  if (!checkBody(req.body, ['email'])) {
-//     res.json({ result: false, error: 'Missing or empty fields' });
-//     return;
-//   }
-
-// });
-// router.get('/profil', (req, res) => { // get profile
-
-// });
-// router.post('/', (req, res) => { // get list
-
-// });
-// router.post('/form/p8', (req, res) => { //
-// router.post('/form/p8', (req, res) => { //
-// router.post('/form/p8', (req, res) => { //
-// router.post('/form/p8', (req, res) => { //
-// router.post('/form/p8', (req, res) => { //
-// router.post('/form/p8', (req, res) => { //
-// router.post('/form/p8', (req, res) => { //
-// router.post('/form/p8', (req, res) => { //
-
-// router.post('/tempcheck', (req, res) => {//route post endpoint /signup
-//   User.findOne({ email: req.body.email }).then(data => { //recherche dans la db User UN seul User avec le nom en param
-//     if (!checkBody(req.body, ['email'])) { //fonction checkBody qui verifie que tout le champs soit ni null ni une string vide prend en parametre du body ['username', 'password']
-//     res.json({ result: false, error: 'Missing or empty fields' });//la fun renvoi un json resiltat false error: 'Missing or empty fields'
-//     return;
-//   }
-//       if (data === null) {
-//         // res.set({
-//         //   'Content-Type': 'text/plain',
-//         //   'Content-Length': '123',
-//         //   ETag: '12345'
-//         //   })
-//         res.redirect('/form/p1')
-//         // res.location('/form/p1')
-
-//       }else{res.json({ result: false, error: 'User already exists' });//doc json qui mentionne que l'user a deja ete trouvé dans la db
-// }
-// })
-// })
-
-// router.get('/canBookmark/:token', (req, res) => {
-//   User.findOne({ token: req.params.token }).then(data => {
-//     if (data) {
-//       res.json({ result: true, canBookmark: data.canBookmark });
-//     } else {
-//       res.json({ result: false, error: 'User not found' });
-//     }
-//   });
-// });
 
 //route geoloc
 router.post("/geoloc", (req, res) => 
@@ -269,13 +90,14 @@ router.post("/geoloc", (req, res) =>
   // Récupération des données envoyées dans le body de la requête
   let {token, lat, lon} = req.body
 
-  //recherche de la ville via latitude et longitude en fasant une requete à l' api gratuite du gouv
+  //recherche de la ville via latitude et longitude en fasant une requete à l' api gratuite 
   fetch(`https://wttr.in/${lat},${lon}?format=j1`).then(r=>r.json()).then(data=>
   {
    
     //verifier que tout les champs sont présents
     if(checkBody(req.body, ["token","lat", "lon"]))
     {
+      //si la reponse de l' api est pas null
       if(data)
       {
        
@@ -289,6 +111,7 @@ router.post("/geoloc", (req, res) =>
              //verifier que l' element user a été bien modifié
               if(userData.modifiedCount>0)
               {
+                //reponse envoyée 
                 res.json({result:true, data:userData})
 
               }
@@ -346,8 +169,7 @@ router.post("/dashboard", (req, res) =>
            //rechercher l' activité choisi dans la collection "activities"
             Activity.findOne({title:userData.sportPlayed[0].title}).then(activityData=>
             {
-              // res.json({result:true, data:activityData})
-
+             
               //verifier que l' activity a été bien trouvée
               if(activityData)
               {
@@ -440,16 +262,6 @@ router.post("/onboarding", (req, res) =>
               return 
             }
 
-            // //recherche du title du niveau
-            // let levelId
-            // for(let Vlevel of sportData.levels)
-            // {
-            //   if(Vlevel.title===levelTitle)
-            //   {
-            //     levelId = Vlevel.title
-            //   }
-            // }
-
             //modification de user pour ajouter les données manquants
             User.updateOne({token:token}, {username:username, name:name, gender:gender, age:age, notificationActive:notificationActive, form:{reason:reason, dayTime:dayTime}, sportPlayed:[sportData._id], currentLevelID:levelId, height:height, weight:weight,city:city.toLowerCase(), photoUrl:"https://res.cloudinary.com/deuhttaaq/image/upload/f_auto,q_auto/v1748005964/projectFinDeBatch/front/images/default-profile_cltqmm.png", stats:{nbSessions:0,totalTime:0}}).then(userData=>
             {
@@ -494,6 +306,7 @@ router.post("/levelupdate", (req, res)=>
       //verifier que l' element user a été bien modifié
       if(modifiedUser.modifiedCount>0)
       { 
+            //recherche du user modifié dans la collection User
             User.findOne({token:token}).populate("sportPlayed").then(userData=>
             {
                 //verifier si le user modifié a été bien trouvé
@@ -563,9 +376,10 @@ router.post("/getsport", (req, res)=>
   if(checkBody(req.body, ["sport"]))
   {
     
-
+    //recherche du sport choisi dans la collection activity
     Activity.findOne({title:sport}).then(dataActivity=>
     {
+      //reponse si le sport a ete bien trouvé
       res.json({result:true, data:dataActivity})
     })
 
